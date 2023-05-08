7281f0a93ced992c809bbbb9778b40e91cbf57ca
Joel Martínez Montavez <joel.martinezmontavez@gmail.com>
2023-05-08 21:17:18 +0200

    Update Animaciones
    Añadidas las animaciones de:
    -Crouch (Agacharse)
    -Jump (Salto)
    -ShowSword (En Proceso)

--- Assets/Scripts/mainCharacterScript.cs
+++ Assets/Scripts/mainCharacterScript.cs
@@ -17,11 +17,13 @@ public class mainCharacterScript : MonoBehaviour
 
     private Rigidbody2D Rigidbody2D;
     private Animator Animator;
+    private CapsuleCollider2D CapsuleCollider;
     
     public bool isGrounded;
     public bool jumpPressed;
     public bool hasJumped;
     private bool isRunning;
+    private bool isCrouching;
     private bool isDoubleTap = false;
     private float horizontal;
     private float lastTapTime = 0f;
@@ -37,6 +39,7 @@ public class mainCharacterScript : MonoBehaviour
     {
         Rigidbody2D = GetComponent<Rigidbody2D>();
         Animator = GetComponent<Animator>();
+        CapsuleCollider = GetComponent<CapsuleCollider2D>();
     }
 
     /**
@@ -78,19 +81,16 @@ public class mainCharacterScript : MonoBehaviour
 
         jumpPressed = Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space);
         
-        if (Physics2D.Raycast(transform.position, Vector3.down, rayCastSize)) 
-        { 
-            isGrounded = true; 
-        } 
-        else 
-        { 
-            isGrounded = false; 
-        }
+        RaycastHit2D raycastHit2D = Physics2D.Raycast(transform.position, Vector2.down, rayCastSize);
 
-        if (jumpPressed && Physics2D.Raycast(transform.position, Vector3.down, rayCastSize) && !hasJumped)
+        if (raycastHit2D.collider != null)
         {
-            Jump();
-            hasJumped = true;
+            isGrounded = true;
+            Animator.SetBool("Jumping", false);
+        }
+        else
+        {
+            isGrounded = false;
         }
 
         if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow) )
@@ -108,6 +108,26 @@ public class mainCharacterScript : MonoBehaviour
 
             lastTapTime = Time.time;
         }
+        if (isGrounded)
+        {
+            isCrouching = Input.GetKey(KeyCode.S);
+            Animator.SetBool("Crouching", isCrouching);
+        }
+
+
+        // if isCrouching is true, then the player cannot move
+        if (isCrouching)
+        {
+            horizontal = 0;
+            jumpPressed = false;
+            // make the player's collider shorter
+            CapsuleCollider.size = new Vector2(0.1f, 0.15f);
+        } 
+        else
+        {
+            // make the player's collider taller
+            CapsuleCollider.size = new Vector2(0.1f, 0.25f);
+        }
 
         if (Input.GetKeyDown(KeyCode.K)) 
         {
@@ -116,19 +136,37 @@ public class mainCharacterScript : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.L)) 
         {
-            health -= damage;
+            TakeDamage(damage);
             mana -= damage;
             stamina -= damage;
         }
+
+        // if (Input.GetKeyDown(KeyCode.Z) && !isAttacking)
+        // {
+        //      Attack();
+        // }
+
+        if (jumpPressed && Physics2D.Raycast(transform.position, Vector3.down, rayCastSize) && !hasJumped)
+        {
+            Jump();
+            hasJumped = true;
+        }
+
+        // if (input.GetKeyDown(KeyCode.Q))
+        // {
+        //     Animator.SetBool("ShowSword", );
+        // }
     }
 
     /**
      * Method to jump
      * - Add force to the player
+     * - Set the jump animation to false
      */
     private void Jump()
     {
         Rigidbody2D.AddForce(Vector2.up * jumpForce);
+        Animator.SetBool("Jumping", true);
         
     }
 
@@ -160,4 +198,50 @@ public class mainCharacterScript : MonoBehaviour
     {
         hasJumped = false;
     }
+
+    /**
+     * Method to check if the player has collided with something
+     * - Check if the player has collided with something
+         * [IF has collided with something] - Set the has jumped to false
+     */
+
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    
+    private void Die()
+    {
+        health -= damage;
+
+        // Animator.SetBool("Die", true);
+
+        // return to main menu
+        Debug.Log("You died!");
+        health = 10;
+    }
+
+    // private void Attack()
+    // {
+    //     Animator.SetTrigger("Attack");
+    //     isAttacking = true;
+
+    //     Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayer);
+
+    //     foreach (Collider2D enemy in hitEnemies)
+    //     {
+    //         enemy.GetComponent<enemyScript>().TakeDamage(swordDamage);
+    //     }
+    // }
+
+    // public void EndAttack()
+    // {
+    //     isAttacking = false;
+    // }
 }
