ec78c172a26bb7349da374def1a2f39e0100966e
Izan Estirado Alfaro <40562916+AstroIzan@users.noreply.github.com>
2023-05-27 03:19:07 +0200

    Escenary Update

--- Assets/Scripts/startSession.cs
+++ Assets/Scripts/startSession.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Networking;
 
 public class startSession : MonoBehaviour
 {
-    // Variables
+        // Variables
     private GameObject modalAlertSession;
     private CanvasGroup canvasGroup;
+
+    public string loginUrl = "http://localhost/PF-DigitalDreams-WebDigitalDreams/html/login.php?location=2"; // URL de inicio de sesión de tu página web
+
 
     /**
      * Method to start the script
@@ -69,6 +73,60 @@ public class startSession : MonoBehaviour
      */
     public void acceptModalAlertGame()
     {
-        SceneManager.LoadScene("mainMenuScene");
+        SceneManager.LoadScene("gameScene");
+    }
+
+    /**
+     * Method to loggin session into dddevelops web.
+     */
+    public IEnumerator CheckLoginStatus()
+    {
+        Debug.Log("Comprobando el estado de inicio de sesión...");
+        // Envía la solicitud GET al servidor
+        UnityWebRequest www = UnityWebRequest.Get(loginUrl);
+
+        // Espera la respuesta del servidor
+        yield return www.SendWebRequest();
+
+        if (www.result != UnityWebRequest.Result.Success)
+        {
+            Debug.Log("Error al obtener el estado de inicio de sesión: " + www.error);
+        }
+        else
+        {
+            // Obtiene la respuesta del servidor
+            string response = www.downloadHandler.text;
+
+            // Analiza la respuesta JSON
+            LoginResponse loginResponse = JsonUtility.FromJson<LoginResponse>(response);
+
+            // Realiza el procesamiento de la respuesta aquí según tus necesidades
+            if (loginResponse.status == "success")
+            {
+                Debug.Log("El usuario " + loginResponse.username + " ha iniciado sesión");
+                // Aquí puedes realizar acciones adicionales cuando el usuario ha iniciado sesión en la página web.
+            }
+            else if (loginResponse.status == "error")
+            {
+                Debug.Log("Error en el inicio de sesión: " + loginResponse.message);
+                // Aquí puedes realizar acciones adicionales cuando se produce un error en el inicio de sesión.
+            }
+            else
+            {
+                Debug.Log("Respuesta desconocida del servidor: " + response);
+                // Aquí puedes manejar otros casos de respuesta desconocida según tus necesidades.
+            }
+        }
+    }
+
+    /**
+     * Simple method to get the json response from the server.
+     */
+    [System.Serializable]
+    public class LoginResponse
+    {
+        public string status;
+        public string message;
+        public string username;
     }
 }
